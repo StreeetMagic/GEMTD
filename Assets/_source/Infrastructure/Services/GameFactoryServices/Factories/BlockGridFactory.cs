55b0f6342756eb9a929af70e28f708c9ddbbde71
Vlad Parastaev <streeetmagic@gmail.com>
2023-11-26 13:28:26 +0400

    Refactor

---
+++ Assets/_source/Infrastructure/Services/GameFactoryServices/Factories/BlockGridFactory.cs
@@ -0,0 +1,73 @@
+﻿using Games;
+using Games.Config.Resources;
+using InfastuctureCore.Services.AssetProviderServices;
+using Infrastructure.Services.CurrentDataServices;
+using UnityEngine;
+
+namespace Infrastructure.Services.GameFactoryServices
+{
+    public class BlockGridFactory
+    {
+        private readonly IAssetProviderService _assetProviderService;
+        private readonly IStaticDataService _staticDataService;
+        private readonly ICurrentDataService _currentDataService;
+
+        public BlockGridFactory(IAssetProviderService assetProviderService, IStaticDataService staticDataService, ICurrentDataService currentDataService)
+        {
+            _assetProviderService = assetProviderService;
+            _staticDataService = staticDataService;
+            _currentDataService = currentDataService;
+        }
+
+        public void CreateBlockGrid()
+        {
+            BlockGridData blockGridData = _currentDataService.Register(CreateBlockGridData());
+            var blockGridView = _assetProviderService.Instantiate<BlockGridView>(Constants.AssetsPath.Prefabs.BlockGrid);
+            CellView[] cellViews = CreateBlockCells(blockGridData.CellDatas, blockGridView.CellsContainer.transform);
+            blockGridView.Init(blockGridData, cellViews);
+        }
+
+        private CellView[] CreateBlockCells(CellData[] cellDatas, Transform parent)
+        {
+            CellView[] cellViews = new CellView[cellDatas.Length];
+
+            for (int i = 0; i < cellDatas.Length; i++)
+            {
+                CellData cellData = cellDatas[i];
+                var position = new Vector3(cellData.Coordinates.X, 0, cellData.Coordinates.Z);
+                var cellView = _assetProviderService.Instantiate<CellView>(Constants.AssetsPath.Prefabs.Cell, position);
+                cellView.transform.SetParent(parent);
+                cellViews[i] = cellView;
+                var block = CreateBlock(position, cellView.transform);
+                cellView.Init(block);
+            }
+
+            return cellViews;
+        }
+
+        private BlockView CreateBlock(Vector3 at, Transform parent)
+        {
+            var blockView = _assetProviderService.Instantiate<BlockView>(Constants.AssetsPath.Prefabs.Block, at);
+            blockView.transform.SetParent(parent);
+            return blockView;
+        }
+
+        private BlockGridData CreateBlockGridData()
+        {
+            int xSize = _staticDataService.Get<GameConfig>().FieldXSize;
+            int ySize = _staticDataService.Get<GameConfig>().FieldYSize;
+
+            CellData[] cellDatas = new CellData[xSize * ySize];
+
+            int count = 0;
+
+            for (int i = 0; i < xSize; i++)
+            {
+                for (int j = 0; j < ySize; j++)
+                    cellDatas[count++] = new CellData(new Coordinates(i, j));
+            }
+
+            return new BlockGridData(cellDatas);
+        }
+    }
+}
--- Assets/_source/Infrastructure/Services/GameFactoryServices/GameFactoryService.cs
+++ Assets/_source/Infrastructure/Services/GameFactoryServices/GameFactoryService.cs
@@ -1,9 +1,6 @@
-﻿using Games;
-using Games.Config.Resources;
-using InfastuctureCore.Services;
+﻿using InfastuctureCore.Services;
 using InfastuctureCore.Services.AssetProviderServices;
 using Infrastructure.Services.CurrentDataServices;
-using UnityEngine;
 
 namespace Infrastructure.Services.GameFactoryServices
 {
@@ -18,62 +15,20 @@ namespace Infrastructure.Services.GameFactoryServices
         private readonly IStaticDataService _staticDataService;
         private readonly ICurrentDataService _currentDataService;
 
+        private readonly BlockGridFactory _blockGridFactory;
+
         public GameFactoryService(IAssetProviderService assetProviderService, IStaticDataService staticDataService, ICurrentDataService currentDataService)
         {
             _assetProviderService = assetProviderService;
             _staticDataService = staticDataService;
             _currentDataService = currentDataService;
+
+            _blockGridFactory = new BlockGridFactory(assetProviderService, staticDataService, currentDataService);
         }
 
         public void CreateBlockGrid()
         {
-            BlockGridData blockGridData = _currentDataService.Register(CreateBlockGridData());
-            var blockGridView = _assetProviderService.Instantiate<BlockGridView>(Constants.AssetsPath.Prefabs.BlockGrid);
-            CellView[] cellViews = CreateBlockCells(blockGridData.CellDatas, blockGridView.CellsContainer.transform);
-            blockGridView.Init(blockGridData, cellViews);
-        }
-
-        private CellView[] CreateBlockCells(CellData[] cellDatas, Transform parent)
-        {
-            CellView[] cellViews = new CellView[cellDatas.Length];
-
-            for (int i = 0; i < cellDatas.Length; i++)
-            {
-                CellData cellData = cellDatas[i];
-                var position = new Vector3(cellData.Coordinates.X, 0, cellData.Coordinates.Z);
-                var cellView = _assetProviderService.Instantiate<CellView>(Constants.AssetsPath.Prefabs.Cell, position);
-                cellView.transform.SetParent(parent);
-                cellViews[i] = cellView;
-                var block = CreateBlock(position, cellView.transform);
-                cellView.Init(block);
-            }
-
-            return cellViews;
-        }
-
-        private BlockView CreateBlock(Vector3 at, Transform parent)
-        {
-            var blockView = _assetProviderService.Instantiate<BlockView>(Constants.AssetsPath.Prefabs.Block, at);
-            blockView.transform.SetParent(parent);
-            return blockView;
-        }
-
-        private BlockGridData CreateBlockGridData()
-        {
-            int xSize = _staticDataService.Get<GameConfig>().FieldXSize;
-            int ySize = _staticDataService.Get<GameConfig>().FieldYSize;
-
-            CellData[] cellDatas = new CellData[xSize * ySize];
-
-            int count = 0;
-
-            for (int i = 0; i < xSize; i++)
-            {
-                for (int j = 0; j < ySize; j++)
-                    cellDatas[count++] = new CellData(new Coordinates(i, j));
-            }
-
-            return new BlockGridData(cellDatas);
+            _blockGridFactory.CreateBlockGrid();
         }
     }
 }
