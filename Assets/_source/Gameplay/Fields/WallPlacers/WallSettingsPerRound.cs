798c1d16e0179ff5c356cdd228f0972de9bc7159
Vlad Parastaev <streeetmagic@gmail.com>
2023-12-04 02:08:47 +0400

    AddedTowers

---
+++ Assets/_source/Gameplay/Fields/WallPlacers/WallSettingsPerRound.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Gameplay.Fields.Cells;
+
+namespace Gameplay.Fields.WallPlacers
+{
+    [Serializable]
+    public class WallSettingsPerRound
+    {
+        public int RoundNumber;
+        public List<Coordinates> DestroyList = new List<Coordinates>();
+        public List<Coordinates> PlaceList = new List<Coordinates>();
+
+        public int PlaceCount => PlaceList.Count;
+
+        public WallSettingsPerRound(int roundNumber)
+        {
+            RoundNumber = roundNumber;
+        }
+    }
+}
