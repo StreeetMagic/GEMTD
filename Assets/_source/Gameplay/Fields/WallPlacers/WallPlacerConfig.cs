9f6f7fecdf2c6d3971abba63b6bb678ec19a0ed5
Vlad Parastaev <streeetmagic@gmail.com>
2023-12-04 02:08:47 +0400

    AddedTowers

--- Assets/_source/Gameplay/Fields/WallPlacers/WallPlacerConfig.cs
+++ Assets/_source/Gameplay/Fields/WallPlacers/WallPlacerConfig.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using Gameplay.Fields.Cells;
 using InfastuctureCore.Services;
@@ -10,6 +9,7 @@ namespace Gameplay.Fields.WallPlacers
     [CreateAssetMenu(fileName = "WallPlacerConfig", menuName = "Configs/WallPlacerConfig")]
     public class WallPlacerConfig : ScriptableObject, IStaticData
     {
+        public int SAVER;
         public int towerPerRound = 5;
         public List<WallSettingsPerRound> WallSettingsPerRounds = new();
 
@@ -17,17 +17,17 @@ namespace Gameplay.Fields.WallPlacers
         {
             if (WallSettingsPerRounds.Count == 0)
             {
-                WallSettingsPerRounds.Add(new WallSettingsPerRound());
+                WallSettingsPerRounds.Add(new WallSettingsPerRound(1337));
             }
 
             WallSettingsPerRound lastWallSettingsPerRound = WallSettingsPerRounds.LastOrDefault();
 
             if (lastWallSettingsPerRound.PlaceCount >= towerPerRound)
             {
-                WallSettingsPerRounds.Add(new WallSettingsPerRound());
+                WallSettingsPerRounds.Add(new WallSettingsPerRound(1337));
                 lastWallSettingsPerRound = WallSettingsPerRounds.Last();
             }
-            
+
             lastWallSettingsPerRound.PlaceList.Add(coordinates);
         }
 
@@ -37,20 +37,11 @@ namespace Gameplay.Fields.WallPlacers
 
             if (lastWallSettingsPerRound.PlaceCount >= towerPerRound)
             {
-                WallSettingsPerRounds.Add(new WallSettingsPerRound());
+                WallSettingsPerRounds.Add(new WallSettingsPerRound(1337));
                 lastWallSettingsPerRound = WallSettingsPerRounds.Last();
             }
 
             lastWallSettingsPerRound.DestroyList.Add(coordinates);
-        }
-
-        [Serializable]
-        public class WallSettingsPerRound
-        {
-            public int RoundNumber;
-            public List<Coordinates> DestroyList = new List<Coordinates>();
-            public List<Coordinates> PlaceList = new List<Coordinates>();
-            public int PlaceCount => PlaceList.Count;
         }
     }
 }
