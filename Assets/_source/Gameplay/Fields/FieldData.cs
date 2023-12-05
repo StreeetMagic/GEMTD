e60abeedad2bbf9dc9ab47a883eccd64b4b378fd
Vlad Parastaev <streeetmagic@gmail.com>
2023-12-05 10:31:19 +0400

    Done

--- Assets/_source/Gameplay/Fields/FieldData.cs
+++ Assets/_source/Gameplay/Fields/FieldData.cs
@@ -20,6 +20,35 @@ namespace Gameplay.Fields
         public CellData[] CellDatas => _cellDatas.ToArray();
         private IStaticDataService StaticDataService => ServiceLocator.Instance.Get<IStaticDataService>();
 
+        public Coordinates[] GetCentralWalls(int towerPerRound)
+        {
+            int size = StaticDataService.Get<FieldConfig>().FieldSize;
+            int centralCoordinate = size / 2 + 1;
+            List<Coordinates> coordinates = new List<Coordinates>();
+
+            while (coordinates.Count < towerPerRound)
+            {
+                for (int i = 0; i < centralCoordinate; i++)
+                {
+                    FindValidCoordinates(centralCoordinate, i, coordinates, towerPerRound);
+
+                    if (coordinates.Count == towerPerRound)
+                    {
+                        if (HasSameCoordinates(coordinates.ToArray()))
+                        {
+                            coordinates.Clear();
+                        }
+                        else
+                        {
+                            return coordinates.ToArray();
+                        }
+                    }
+                }
+            }
+
+            return coordinates.ToArray();
+        }
+
         public CellData GetCellData(Coordinates coordinates)
         {
             foreach (CellData cellData in _cellDatas)
@@ -33,46 +62,25 @@ namespace Gameplay.Fields
             return null;
         }
 
-        public Coordinates[] GetCentralWalls(int towerPerRound)
+        private void FindValidCoordinates(int centralCoordinate, int i, List<Coordinates> coordinates, int towerPerRound)
         {
-            int size = StaticDataService.Get<FieldConfig>().FieldSize;
-
-            int centralCoordinate = size / 2 + 1;
-
-            List<Coordinates> coordinates = new List<Coordinates>();
-
-            while (coordinates.Count < towerPerRound)
+            for (int x = centralCoordinate - i; x < centralCoordinate + i; x++)
             {
-                for (int i = 0; i < centralCoordinate; i++)
+                for (int z = centralCoordinate - i; z < centralCoordinate + i; z++)
                 {
-                    for (int x = centralCoordinate - i; x < centralCoordinate + i; x++)
+                    CellData cellData = GetCellData(new Coordinates(x, z));
+
+                    if (cellData.CanBeReplacedWithTower)
                     {
-                        for (int z = centralCoordinate - i; z < centralCoordinate + i; z++)
-                        {
-                            CellData cellData = GetCellData(new Coordinates(x, z));
+                        coordinates.Add(new Coordinates(x, z));
+                    }
 
-                            if (cellData.CanBeReplacedWithTower)
-                            {
-                                coordinates.Add(new Coordinates(x, z));
-                            }
-
-                            if (coordinates.Count == towerPerRound)
-                            {
-                                if (HasSameCoordinates(coordinates.ToArray()))
-                                {
-                                    coordinates.Clear();
-                                }
-                                else
-                                {
-                                    return coordinates.ToArray();
-                                }
-                            }
-                        }
+                    if (coordinates.Count == towerPerRound)
+                    {
+                        return;
                     }
                 }
             }
-
-            return coordinates.ToArray();
         }
 
         private bool HasSameCoordinates(Coordinates[] coordinates)
