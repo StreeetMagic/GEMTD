206237df325bb222977072db6c9fa6fdde6f5386
Vlad Parastaev <streeetmagic@gmail.com>
2023-12-07 14:23:30 +0400

    Enemy move

--- Assets/_source/Gameplay/Fields/EnemySpawners/Enemies/EnemyModel.cs
+++ Assets/_source/Gameplay/Fields/EnemySpawners/Enemies/EnemyModel.cs
@@ -1,5 +1,9 @@
-﻿using Gameplay.Fields.EnemySpawners.Enemies.Movers;
+﻿using System.Collections.Generic;
+using Gameplay.Fields.Checkpoints;
+using Gameplay.Fields.EnemySpawners.Enemies.Movers;
+using InfastuctureCore.ServiceLocators;
 using InfastuctureCore.Utilities;
+using Infrastructure.Services.CurrentDataServices;
 using UnityEngine;
 
 namespace Gameplay.Fields.EnemySpawners.Enemies
@@ -9,9 +13,28 @@ namespace Gameplay.Fields.EnemySpawners.Enemies
         public ReactiveProperty<float> Health { get; } = new ReactiveProperty<float>();
         public EnemyMoverModel MoverModel { get; set; }
 
+        private ICurrentDataService CurrentDataService => ServiceLocator.Instance.Get<ICurrentDataService>();
+
         public EnemyModel(Vector3 position)
         {
-            MoverModel = new EnemyMoverModel(position);
+            List<Vector3> checkPoints = GetCheckPoints();
+
+            MoverModel = new EnemyMoverModel(position, checkPoints.ToArray());
+        }
+
+        private List<Vector3> GetCheckPoints()
+        {
+            CheckPointModel[] positions = CurrentDataService.FieldModel.GetCheckPointModels();
+
+            List<Vector3> checkPoints = new();
+
+            foreach (var positionModel in positions)
+            {
+                checkPoints.Add(positionModel.CellModel.Position);
+            }
+
+            Debug.Log(checkPoints.Count);
+            return checkPoints;
         }
 
         public void TakeDamage(float damage)
--- Assets/_source/Gameplay/Fields/EnemySpawners/Enemies/Movers/EnemyMoverModel.cs
+++ Assets/_source/Gameplay/Fields/EnemySpawners/Enemies/Movers/EnemyMoverModel.cs
@@ -4,13 +4,15 @@ namespace Gameplay.Fields.EnemySpawners.Enemies.Movers
 {
     public class EnemyMoverModel
     {
-        public EnemyMoverModel(Vector3 position)
+        public EnemyMoverModel(Vector3 position, Vector3[] checkPoints)
         {
             Position = position;
+            CheckPoints = checkPoints;
         }
 
         public Vector3 Position { get; set; }
         public float Speed { get; set; } = 5f;
+        public Vector3[] CheckPoints { get; set; }
 
         public void Move(Vector3 position)
         {
