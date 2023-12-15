e0f5eb4749b70cb31eb86db63e29b0f97a487f0d
Vlad Parastaev <streeetmagic@gmail.com>
2023-12-15 17:57:30 +0400

    Bug

--- Assets/_source/Infrastructure/GameLoopStateMachines/States/EnemyMoveState.cs
+++ Assets/_source/Infrastructure/GameLoopStateMachines/States/EnemyMoveState.cs
@@ -19,7 +19,8 @@ namespace Infrastructure.GameLoopStateMachines.States
 
         public void Enter()
         {
-            CurrentDataService.FieldModel.EnemySpawnerModel.Spawn(() => { _gameLoopStateMachine.Enter<PlaceWallsState>(); });
+            //CurrentDataService.FieldModel.EnemySpawnerModel.Spawn(() => { _gameLoopStateMachine.Enter<PlaceWallsState>(); });
+            _gameLoopStateMachine.Enter<PlaceWallsState>();
         }
 
         public void Exit()
